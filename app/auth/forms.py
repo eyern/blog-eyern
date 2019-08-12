7a0bdabef3a78b96596996d66d1488fb008c512d
eyern <eyernfuture@gmail.com>
2019-08-12 22:24:21 +0300

    include the string field for passwords input and match

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,21 +9,21 @@ class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address', validators=[Required(), Email()])
     username = StringField('Enter your username', validators=[Required()])
     password = PasswordField('Password', validators=[Required(),
-                                                     EqualTo('password_confirm', message='Passwords must match')])
-    password_confirm = PasswordField('Confirm Passwords', validators=[Required()])
+                                                     EqualTo('password_confirm', message='Must match')])
+    password_confirm = PasswordField('Confirm Your Password', validators=[Required()])
     submit = SubmitField('Sign Up')
 
     def validate_email(self, data_field):
         if User.query.filter_by(email=data_field.data).first():
-            raise ValidationError('There is an account with that email')
+            raise ValidationError('The email you provided is taken')
 
     def validate_username(self, data_field):
         if User.query.filter_by(username=data_field.data).first():
-            raise ValidationError('That username is taken')
+            raise ValidationError('username taken')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Your Email Address', validators=[Required(), Email()])
     password = PasswordField('Password', validators=[Required()])
-    remember = BooleanField('Remember me')
+    remember = BooleanField('Save password')
     submit = SubmitField('Sign In')

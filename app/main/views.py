1afa55403204882eb6886432c9aa745bff2c1426
eyern <eyernfuture@gmail.com>
2019-08-12 22:26:05 +0300

    add a log in function

--- app/main/views.py
+++ app/main/views.py
@@ -11,7 +11,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 def index():
     """View root page function that returns index page and the various news sources"""
 
-    title = 'Home- Welcome to Blogger'
+    title = 'Home- Start Your Blog'
 
     form = PostForm()
 
